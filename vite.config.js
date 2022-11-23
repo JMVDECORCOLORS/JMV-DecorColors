38956d5f37c4ff12301a638faa444377d52a711a
Victor Basso <66968560+VictorBasso36@users.noreply.github.com>
2022-11-23 16:02:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Decor-Colors/',
+  base: '/JMV-DecorColors/',
   plugins: [react()]
 })
